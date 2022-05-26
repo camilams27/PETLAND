ac547ed6c87b29d3a2a3f850c0df5dac88e179e6
Camila Silva <silva.camilam27@gmail.com>
2022-05-26 19:22:42 -0300

    create new page

--- petland-app/src/app/app-routing.module.ts
+++ petland-app/src/app/app-routing.module.ts
@@ -2,24 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-
-import { AgendaComponent } from './components/agenda/agenda.component';
-import { LocaisComponent } from './components/locais/locais.component';
-import { SaudeComponent } from './components/saude/saude.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AppComponent
-  },
-  // {
-  //   path: 'cadastro',
-  //   component: 
-  // },
-  {
-    path: 'login',
-    component: LoginPageComponent
   }
 ];
 
--- petland-app/src/app/app.module.ts
+++ petland-app/src/app/app.module.ts
@@ -3,13 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponentComponent } from './template/header-component/header-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { ContentComponentComponent } from './template/content-component/content-component.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { FooterComponentComponent } from './template/footer-component/footer-component.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
@@ -17,23 +11,12 @@ import {MatCardModule} from '@angular/material/card';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
-import { AgendaComponent } from './components/agenda/agenda.component';
-import { SaudeComponent } from './components/saude/saude.component';
-import { LocaisComponent } from './components/locais/locais.component';
-
-
+import { HeaderComponent } from './template/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponentComponent,
-    ContentComponentComponent,
-    FooterComponentComponent,
-    HomePageComponent,
-    LoginPageComponent,
-    AgendaComponent,
-    SaudeComponent,
-    LocaisComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
