ea63d863260c463047febe36ccdeaab89d13d4b2
Camila Silva <silva.camilam27@gmail.com>
2022-05-26 16:34:41 -0300

    atualizacao da tela inicial

--- petland-app/src/app/app-routing.module.ts
+++ petland-app/src/app/app-routing.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+
 import { AgendaComponent } from './components/agenda/agenda.component';
 import { LocaisComponent } from './components/locais/locais.component';
-
-import { LoginPageComponent } from './components/login-page/login-page.component';
 import { SaudeComponent } from './components/saude/saude.component';
 
 const routes: Routes = [
   {
-    path:'',
+    path: '',
     component: AppComponent
+  },
+  // {
+  //   path: 'cadastro',
+  //   component: 
+  // },
+  {
+    path: 'login',
+    component: LoginPageComponent
   }
-  
-  
 ];
 
 @NgModule({
