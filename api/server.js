0632bd564771cb15f98960c22ba4de046a217206
Camila Silva <silva.camilam27@gmail.com>
2022-06-02 20:52:14 -0300

    subindo link do colab referente a cnn de gato e cachorro e alteracao na api

--- api/server.js
+++ api/server.js
@@ -8,13 +8,9 @@ connectDatabase();
 
 const port = 3333;
 
+app.use(cors());
+
 app.use(express.json())
 app.use(routes);
-
-var corsOptions = {
-    origin: "http://localhost:3333/",
-    optionsSuccessStatus: 200,
-  };
-app.use(cors(corsOptions));
 
 app.listen(port, ()=>{ console.log(`acesse: http://localhost:${port}`)});
