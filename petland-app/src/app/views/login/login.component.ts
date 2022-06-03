4d9c694087c88f4a7ab8a1554223846f768d02e1
Camila Silva <silva.camilam27@gmail.com>
2022-06-03 10:09:26 -0300

    "atualizando paginas "

--- petland-app/src/app/views/login/login.component.ts
+++ petland-app/src/app/views/login/login.component.ts
@@ -6,13 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-<<<<<<< HEAD
   hide = true;
-=======
-
-  hide = true;
-
->>>>>>> texto
   constructor() { }
 
   ngOnInit(): void {
