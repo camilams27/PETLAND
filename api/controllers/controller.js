7ecfac52cb0a8a826f38bb700bacbc93b3cd0b60
Camila Silva <silva.camilam27@gmail.com>
2022-06-30 22:22:40 -0300

    atualização das rotas e controllers

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -2,18 +2,16 @@ const { response } = require('express');
 
 const Client = require('../models/Client');
 
-
-
 module.exports = {
-    async clients(request, response){
-        try{
+    async clients(request, response) {
+        try {
             const clients = await Client.find();
-            return response.status(200).json( {clients} )
-        }catch (err){
-            response.status(500).json( {error: err.message})
+            return response.status(200).json({ clients });
+        } catch (err) {
+            response.status(500).json({ error: err.message });
         }
     },
-    async createClient(request, response){
+    async createClient(request, response) {
         const { 
             login, 
             senha,
@@ -23,8 +21,8 @@ module.exports = {
             dataNasc
         } = request.body;
 
-        if( !login || !senha || !nome){
-            return response.status(400).json({ error: 'login ou senha inválidos'})
+        if (!login || !senha || !nome) {
+            return response.status(400).json({ error: 'login ou senha inválidos' });
         }
 
         const client = new Client({
@@ -34,42 +32,68 @@ module.exports = {
             email,
             telefone,
             dataNasc
-        })
+        });
 
-        try{
+        try {
             const loginClient = await Client.findOne({ login: login }).exec();
-            if(loginClient) {
-               return response.status(400).json({message: 'login já existente'})
+            if (loginClient) {
+               return response.status(400).json({ message: 'login já existente' });
             }
             await client.save();
-            return response.status(201).json({message: 'usuário adicionado'})
-        }catch (error){
-            response.status(500).json({error: error.message})
+            return response.status(201).json({ message: 'usuário adicionado' });
+        } catch (error) {
+            response.status(500).json({ error: error.message });
         }
     },
-    async updateClient(request, response){
-        try{
+    async updateClient(request, response) {
+        try {
             const { senha, nome } = request.body;
             const { login } = request.params;
-        if(!login && !senha && !nome){
-            return response.status(400).json({error: 'informe os campos'});
+        if(!login && !senha && !nome) {
+            return response.status(400).json({ error: 'informe os campos' });
         }
 
-        await Client.findOneAndUpdate({ login }, { nome, senha })
-        return response.status('200').json({message: 'usuário editado'})
+        await Client.findOneAndUpdate({ login }, { nome, senha });
+        return response.status('200').json({ message: 'usuário editado' });
         
         } catch (error) {
-            response.status(500).json({error: error.message})
+            response.status(500).json({ error: error.message });
         }
         
     },
-    async deleteClient(request, response){
-        try{
+    async deleteClient(request, response) {
+        try {
             const { login } = request.params;
-            await Client.deleteOne({ login })
-            return response.status(200).json({message : 'usuário deletado.'})
-        }catch(error){
-            response.status(500).json({error: error.message})
+            await Client.deleteOne({ login });
+            return response.status(200).json({ message : 'usuário deletado.' });
+        } catch (error) {
+            response.status(500).json({ error: error.message });
+        }
+    },
+    async loginClient(request, response) {
+        try {
+            const { 
+                login, 
+                senha
+            } = request.body;
+
+            if (!login || !senha) {
+                return response.status(200).json({ message: 'informe os campos corretamente.' });
+            }
+
+            const hasLogin = await Client.findOne({ login: login }).exec();
+            if (!hasLogin) {
+               return response.status(200).json({ message: 'login não cadastrado.' });
+            }
+
+            const loginClient = await Client.findOne({ login: login, senha: senha }).exec();
+            if (!loginClient) {
+                return response.status(200).json({ message: 'login ou senha inválidos' });
+            } else {
+                return response.status(200).json({ message : 'login feito com sucesso.', success: true });
+            }
+        } catch (error) {
+            response.status(500).json({ error: error.message });
         }
     }
 }
--- api/routes.js
+++ api/routes.js
@@ -7,6 +7,7 @@ const middleware = require('./middlewares/middleware');
 routes.get('/clients', controller.clients);
 routes.post('/clients', controller.createClient);
 routes.put('/clients/:login', middleware.validateLogin, controller.updateClient);
-routes.delete('/clients/:login', middleware.validateLogin, controller.deleteClient)
+routes.delete('/clients/:login', middleware.validateLogin, controller.deleteClient);
+routes.post('/clients/login', controller.loginClient);
 
 module.exports = routes;
